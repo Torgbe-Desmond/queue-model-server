6e47885ec685589501aab0bb910630078da94df7
Torgbe-Desmond <123torgbe@gmail.com>
2024-10-25 05:47:06 -0700

    Added delete logic for histories

--- routes/history.js
+++ routes/history.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const { createScanHistory, getAllScanHistories } = require('../controllers/history');
+const { createScanHistory, getAllScanHistories,deleteScanHistory } = require('../controllers/history');
 const router = express.Router();
 
 
 router.post('/scan-history',createScanHistory )
 router.get('/get-user-history/:id', getAllScanHistories )
-
+router.delete('/delete-history/:historyId',deleteScanHistory)
 module.exports = router;   
